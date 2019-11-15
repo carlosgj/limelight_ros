675a19263e8a70bb7240159b63fbcb3c5dbec04c
Carlos Gross Jones <c.gross.jones@gmail.com>
2019-11-15 10:34:08 -0800

    Adding additional websocket data

--- scripts/websocketpublish.py
+++ scripts/websocketpublish.py
@@ -3,7 +3,7 @@
 import rospy
 from websocket import create_connection
 from std_msgs.msg import Header, Bool, Float32
-from geometry_msgs.msg import Quaternion
+from geometry_msgs.msg import Quaternion, Transform
 from tf.transformations import quaternion_from_euler
 from limelight_ros.msg import LimelightStamped
 from math import radians
@@ -25,15 +25,18 @@ if __name__ == '__main__':
             assert len(stringparts) ==2
             assert stringparts[0] == "status_update"
             data = stringparts[1].split(':')
+            
+            pipelineLatency = float(data[4])
+            
             msg = LimelightStamped()
             msg.header = Header()
             msg.header.stamp = rospy.Time.now()
 
             tx = float(data[0])
             ty = float(data[1])
-            rot = float(data[3])
+            ts = float(data[3])
             #print tx, ty, rot
-            tf_quat = quaternion_from_euler(radians(tx), radians(ty), radians(rot), 'rxyz')
+            tf_quat = quaternion_from_euler(radians(tx), radians(ty), radians(ts), 'rxyz')
             msg.targetLocation = Quaternion(tf_quat[0], tf_quat[1], tf_quat[2], tf_quat[3])
 
             #Convert from percent
@@ -42,7 +45,28 @@ if __name__ == '__main__':
             msg.hasTargets.data = bool(int(data[5]))
 
             msg.pipeline.data = int(data[6])
-
+            
+            #Don't know what this stuff is
+            ignoringNetworkTables = bool(int(data[7]))
+            imageCount = int(data[8])
+            interfaceNeedsRefresh = bool(int(data[9]))
+            usingGrip = bool(int(data[10]))
+            pipelineImageCount = int(data[11])
+            snapshotMode = int(data[12])
+            hwType = int(data[13])
+            
+            posetX = float(data[14])
+            posetY = float(data[15])
+            posetZ = float(data[16])
+            poserX = float(data[17])
+            poserY = float(data[18])
+            poserZ = float(data[19])
+            tf_quat = quaternion_from_euler(radians(poserX), radians(poserY), radians(poserZ), 'rxyz')
+            msg.cameraPose.translation.x = posetX
+            msg.cameraPose.translation.y = posetY
+            msg.cameraPose.translation.z = posetZ
+            msg.cameraPose.rotation = Quaternion(tf_quat[0], tf_quat[1], tf_quat[2], tf_quat[3])
+            
             pub.publish(msg)
     except:
         ws.close()
